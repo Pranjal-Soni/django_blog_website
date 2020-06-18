b56d970b8e0a34cd39b07be6fc8284c8d28a238e
Pranjal-Soni <thepranjalsoni@gmail.com>
2020-06-18 10:33:42 +0530

    Navbar update and blog Section

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 
+posts =[
+    {
+        'author':'Pranjal',
+        'title': 'Blog Post one',
+        'Content':'FIrst post Content',
+        'Date' : 'May 25, 2020'
+    },
+    {
+        'author':'Pranjal',
+        'title': 'Blog Post two',
+        'Content':'FIrst post Content',
+        'Date' : 'May 25, 2020'
+    },
+    {
+        'author':'Pranjal',
+        'title': 'Blog Post Three',
+        'Content':'FIrst post three',
+        'Date' : 'May 25, 2020'
+    }
+]
+
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        'posts' : posts
+    }
+    return render(request,'blog/home.html',context)
 
 def About(request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html',{'title':'About'})
