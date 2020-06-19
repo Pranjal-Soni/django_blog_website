c62ff5a5cbbbae823b578c9a053bdd7553f8ef7c
Pranjal-Soni <thepranjalsoni@gmail.com>
2020-06-19 10:40:27 +0530

    user registration created

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
-posts =[
-    {
-        'author':'Pranjal',
-        'title': 'Blog Post one',
-        'Content':'FIrst post Content',
-        'Date' : 'May 25, 2020'
-    },
-    {
-        'author':'Pranjal',
-        'title': 'Blog Post two',
-        'Content':'FIrst post Content',
-        'Date' : 'May 25, 2020'
-    },
-    {
-        'author':'Pranjal',
-        'title': 'Blog Post Three',
-        'Content':'FIrst post three',
-        'Date' : 'May 25, 2020'
-    }
-]
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
